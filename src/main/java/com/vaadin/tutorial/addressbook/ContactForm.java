bdd7c8e5496ab680b45072bb566c5c1128d12d98
kunal <rajankunal@gmail.com>
2018-06-09 04:52:04 -0400

    function changed

--- src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
@@ -1,4 +1,4 @@
-package com.vaadin.tutorial.addressbook;
+ppackage com.vaadin.tutorial.addressbook;
 
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.tutorial.addressbook.backend.Contact;
